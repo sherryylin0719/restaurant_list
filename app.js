860814b0bf75ba320146b0431b2634147043caec
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-24 11:33:40 +0900

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require packages used in the project
 const express = require("express")
+const session = require('express-session')
 const exphbs = require("express-handlebars")
 const bodyParser = require('body-parser')
 const methodOverride = require("method-override")
@@ -11,6 +12,11 @@ const app = express()
 // setting template engine
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
